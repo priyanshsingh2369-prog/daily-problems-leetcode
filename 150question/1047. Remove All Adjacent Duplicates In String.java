e86bc7946e8750e715e644b3930517dcda7309d9
priyanshsingh369 <priyanshsingh2369@gmail.com>
2026-07-21 13:29:57 +0530

    alternative remove krna hai

--- 150question/1047. Remove All Adjacent Duplicates In String.java
+++ 150question/1047. Remove All Adjacent Duplicates In String.java
@@ -29,3 +29,33 @@ Constraints:
 
 1 <= s.length <= 105
 s consists of lowercase English letters. */
+class Solution {
+   
+    public String removeDuplicates(String s) {
+        // int i = 0;
+        // int j = 1;
+        // while(i<s.length()&& j< s.length()){
+        //     if(s.charAt(i)==s.charAt(j)){
+        //         // remove(s,i,j);
+        //         s = s.substring(0,i)+s.substring(j+1);
+        //         i=0;
+        //         j=1;
+        //     }else{
+        //         i++;
+        //         j++;
+        //     }
+        // }
+
+
+        StringBuilder st = new StringBuilder();
+        for (char x : s.toCharArray()){
+            if(st.length()>0&&st.charAt(st.length()-1)==x){
+                st.deleteCharAt(st.length()-1);
+            }else{
+                st.append(x);
+            }
+        }
+
+        return st.toString();
+    }
+}
