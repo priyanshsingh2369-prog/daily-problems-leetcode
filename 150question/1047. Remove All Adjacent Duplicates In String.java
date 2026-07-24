fd9527a02ec84f6620c01a88d89806ee68b2ecd4
priyanshsingh369 <priyanshsingh2369@gmail.com>
2026-07-24 14:15:22 +0530

    recurtion is on

--- 150question/1047. Remove All Adjacent Duplicates In String.java
+++ 150question/1047. Remove All Adjacent Duplicates In String.java
@@ -16,7 +16,7 @@ Return the final string after all such duplicate removals have been made. It can
 Example 1:
 
 Input: s = "abbaca"
-Output: "ca"
+Output: "ca"    aans
 Explanation: 
 For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 Example 2:
