59c666cdc7e93d1783c10763ef7b83fc905c1671
priyanshsingh369 <priyanshsingh2369@gmail.com>
2026-07-28 20:11:45 +0530

    recurtion is on

--- 150question/3517. Smallest Palindromic Rearrangement I.java
+++ 150question/3517. Smallest Palindromic Rearrangement I.java
@@ -1,3 +1,76 @@
+/*3517. Smallest Palindromic Rearrangement I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given a palindromic string s.
+
+Return the lexicographically smallest palindromic permutation of s.
+
+ 
+
+Example 1:
+
+Input: s = "z"
+
+Output: "z"
+
+Explanation:
+
+A string of only one character is already the lexicographically smallest palindrome.
+
+Example 2:
+
+Input: s = "babab"
+
+Output: "abbba"
+
+Explanation:
+
+Rearranging "babab" → "abbba" gives the smallest lexicographic palindrome.
+
+Example 3:
+
+Input: s = "daccad"
+
+Output: "acddca"
+
+Explanation:
+
+Rearranging "daccad" → "acddca" gives the smallest lexicographic palindrome.
+
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+s is guaranteed to be palindromic.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+121,259/167.6K
+Acceptance Rate
+72.3%
+Topics
+icon
+Companies
+Hint 1
+Hint 2
+Construct one half (using s), and then the other half is its reverse to obtain the lexicographically smallest permutation.
+Similar Questions
+Discussion (92)
+
+Choose a type
+
+
+
+Copyright © 2026 LeetCode. All rights reserv*/
 class Solution {
     public String smallestPalindrome(String s) {
         int[] freq = new int[26];
