8c8985766a2eeae1ce0e228cf9603e5ee0ccaa93
Priyansh Singh <priyanshsingh2369@gmail.com>
2026-09-09 21:41:58 +0530

    Time: 1 ms (99.36%), Space: 42.5 MB (85.26%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+
+        for (long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
+
+            // Prevent overflow
+            if (x > n / 1000) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
